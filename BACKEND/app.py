fe8e8f69f13d2d7f05099a5aa46f727163f677ec
GhostDevu <devanshuagrawal99@gmail.com>
2024-03-19 15:18:42 +0530

    STARTING THE COMPLETE BACKEND

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -3,20 +3,30 @@ import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
+from datetime import datetime, timedelta
 
 load_dotenv()
 app = Flask(__name__)
 
 DB_URI = os.getenv('DB_URI')
-
+client = MongoClient(DB_URI)
 
 try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
     client = MongoClient(DB_URI)
     db = client['sample_mflix']
     print("Connected to MongoDB successfully!")
 except ConnectionFailure as e:
     print("Could not connect to MongoDB: %s" % e)
 
+curUserId = -1
+checkInDate = datetime.now()
+checkOutDate = datetime.now()
+srt = '0'
+foodId = '0'
+availableOnly = '0'
+roomId = 0
 
 @app.after_request
 def add_cors_headers(response):

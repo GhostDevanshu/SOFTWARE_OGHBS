132f1de69df68a8c60567ad5bb79b2fce68d9b6c
muzanzackson <kondurijeevanvarma777@gmail.com>
2024-03-27 23:16:20 +0530

    intitial commit to frontend(jeevan)

---
+++ FRONTEND/src/components/Navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+      <nav className="navbar">
+        <h1>IIT KGP Guest House </h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/login">Login</a>
+          <a href="/signup">Signup</a>
+          <a href="/about" style={{ 
+            color: 'white', 
+            backgroundColor: '#f1356d',
+            borderRadius: '8px' 
+          }}>About</a>
+        </div>
+      </nav>
+    );
+  }
+   
+  export default Navbar;
